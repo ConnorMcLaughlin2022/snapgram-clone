09f0daa3afcf386e0c5c4fde1feb337aa01389a0
Connor McLaughlin <116329927+ConnorMcLaughlin2022@users.noreply.github.com>
2025-01-04 14:30:28 -0700

    Create PostStats.tsx

---
+++ src/components/shared/PostStats.tsx
@@ -0,0 +1,19 @@
+import { Models } from "appwrite";
+
+type PostStatsProps = {
+    post: Models.Document;
+    userId: string;
+}
+
+const PostStats = ({post , userId } : PostStatsProps) => {
+  return (
+    <div className="flex justify-between items-center z-20">
+        <div className="dflex gap-2 mr-5">
+            <img src="/assets/icons/like.svg" alt="like" width={20 } height={20} onClick={() => {}} className="cursor-pionter" />
+            <p className="small-medium lg:base-medium">0</p>
+        </div>
+    </div>
+  )
+}
+
+export default PostStats
